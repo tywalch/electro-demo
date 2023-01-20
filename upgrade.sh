30bf8718bbdb959d7286e479cf013bd122961382
tywalch <tywalch@gmail.com>
2023-01-19 23:05:04 -0500

    new electrodb build

---
+++ upgrade.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd ../electrodb
+npm run build
+cd ../
+cp -nf ./electrodb/playground/bundle.js ./electro-demo/src/display/electrodb.js
+cd electro-demo
+git add src/display/electrodb.js
+git commit -m "new electrodb build"
+git push
