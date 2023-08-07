0c208d9167cf9c473a9f66367dd98fb07d595174
tywalch <tywalch@gmail.com>
2023-08-07 10:57:46 -0400

    update

--- upgrade.sh
+++ upgrade.sh
@@ -5,5 +5,5 @@ cd ../
 cp -nf ./electrodb/playground/bundle.js ./electro-demo/src/display/electrodb.js
 cd electro-demo
 git add src/display/electrodb.js
-#git commit -m "new electrodb build"
-#git push
+git commit -m "new electrodb build"
+git push
